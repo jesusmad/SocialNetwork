5c70b2b62a33f19638f189cdb0f40a3a1a0171e3
Alberto Pérez <albertoperezsimon@gmail.com>
2018-03-12 11:51:23 +0100

    Auto stash before merge of "develop" and "origin/develop"

--- app/src/main/java/com/althreeus/socialnetwork/views/RegisterActivity.kt
+++ app/src/main/java/com/althreeus/socialnetwork/views/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.althreeus.socialnetwork.views
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.althreeus.socialnetwork.R
@@ -10,5 +11,9 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.register)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
     }
 }
