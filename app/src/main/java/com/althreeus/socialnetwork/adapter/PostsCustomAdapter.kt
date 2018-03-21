88699f7c4bde2b446b78aa98673625dd58ed002e
cralejandro <32743450+cralejandro@users.noreply.github.com>
2018-03-21 19:53:27 +0100

    posts hecho
    # Conflicts:
    #	.gradle/4.1/fileHashes/fileHashes.lock
    #	.gradle/4.1/taskHistory/fileSnapshots.bin
    #	.gradle/4.1/taskHistory/taskHistory.bin
    #	.gradle/4.1/taskHistory/taskHistory.lock
    #	.idea/misc.xml

---
+++ app/src/main/java/com/althreeus/socialnetwork/adapter/PostsCustomAdapter.kt
@@ -0,0 +1,43 @@
+package com.althreeus.socialnetwork.adapter
+
+import android.content.Context
+import android.view.View
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.althreeus.socialnetwork.model.Post
+import com.althreeus.socialnetwork.model.Repository
+import kotlinx.android.synthetic.main.repositories_row.view.*
+
+/**
+ * Created by alberto on 21/03/2018.
+ */
+class PostsCustomAdapter (val context: Context,
+                          val layout: Int,
+                          val data: List<Post>): RecyclerView.Adapter<PostsCustomAdapter.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val viewlayout = layoutInflater.inflate(layout, parent, false)
+        return ViewHolder(viewlayout, context)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = data[position]
+        holder.bind(item)
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+
+    inner class ViewHolder(viewLayout: View, val context: Context): RecyclerView.ViewHolder(viewLayout) {
+        fun bind(dataItem: Post){
+
+
+
+        }
+    }
+}
--- app/src/main/java/com/althreeus/socialnetwork/model/Class.kt
+++ app/src/main/java/com/althreeus/socialnetwork/model/Class.kt
@@ -1,15 +1,17 @@
 package com.althreeus.socialnetwork.model
 
+import java.io.Serializable
+
 
 /**
  * Created by Alejandro on 28/02/2018.
  */
-data class User (val id: Int,val nick: String,val password:String,val email:String,val idGit:Int,val nickGit:String)
-data class Post(val id:Int,val idTopic: Int,val idUser: Int,val content:String,val date:String)
+data class User (val id: Int,val nick: String,val password:String,val email:String,val idGit:Int,val nickGit:String):Serializable
+data class Post(val id:Int,val idTopic: Int,val idUser: Int,val content:String,val date:String):Serializable
 data class Category(val id:Int,val name:String)
-data class Technology(val id:Int,val name:String,val logo:String,val color: String)
-data class Topic(val id: Int,val idUser:Int,val nick:String,val idTechnology:Int,val nameTechnology: String,val idCategory:Int,val nameCategory: String,val date:String)
-data class Response(val users:ArrayList<User>, val categories:ArrayList<Category>, val posts:ArrayList<Post>, val technologies:ArrayList<Technology>, val topics:ArrayList<Topic>, val user: User, val topic: Topic, val post: Post, val technology: Technology, val category: Category)
+data class Technology(val id:Int,val name:String,val logo:String,val color: String):Serializable
+data class Topic(val id: Int,val idUser:Int,val nick:String,val idTechnology:Int,val nameTechnology: String,val idCategory:Int,val nameCategory: String,val date:String,val name: String):Serializable
+data class Response(val users:ArrayList<User>, val categories:ArrayList<Category>, val posts:ArrayList<Post>, val technologies:ArrayList<Technology>, val topics:ArrayList<Topic>, val user: User, val topic: Topic, val post: Post, val technology: Technology, val category: Category):Serializable
 
 
 
